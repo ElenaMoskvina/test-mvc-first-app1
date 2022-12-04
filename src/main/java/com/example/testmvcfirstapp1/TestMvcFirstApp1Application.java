08c68c104da4b695948488ad7da5835808cd938d
unknown <elena.v.msk@gmail.com>
2022-12-04 17:18:28 +0300

    Initial commit

---
+++ src/main/java/com/example/testmvcfirstapp1/TestMvcFirstApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.testmvcfirstapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMvcFirstApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMvcFirstApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testmvcfirstapp1/TestMvcFirstApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmvcfirstapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMvcFirstApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
